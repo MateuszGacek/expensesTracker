55de696d7116f6e8334934440ef19b7f00e5a5e7
Mateusz <mateusz_gacek@interia.pl>
2022-11-08 21:26:48 +0100

    Create navigate layout

---
+++ screens/AllExpenses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function AllExpenses() {
+	return (
+		<View>
+			<Text>allExpenses</Text>
+		</View>
+	);
+}
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function RecentExpenses() {
+	return (
+		<View>
+			<Text>RecentExpe</Text>
+		</View>
+	);
+}
+
+export default RecentExpenses;
